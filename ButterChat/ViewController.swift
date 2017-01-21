ad97bf01a40534ce60d48910dc411b19cc43c5c5
Steven Shang <stevenshang@pal-nat186-65-226.itap.purdue.edu>
2017-01-20 20:18:26 -0600

    test

--- ButterChat/ViewController.swift
+++ ButterChat/ViewController.swift
@@ -20,6 +20,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var someButton: UIButton!
+    @IBAction func someButtonPressed(_ sender: Any) {
+        
+    }
+    
+    func sendRequest() {
+        
+    }
 
 }
 
